1150ab2394ec97e6167ac70579d43235aeb1e831
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-27 13:18:25 +0200

    navigationbar added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './components/Login'
 import HomeScreen from './components/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <HomeScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,9 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './Login'
 
 export default function HomeScreen() {
   return (
-    <Login />
+    <Text>Tänään on pvm</Text>
   );
 }
