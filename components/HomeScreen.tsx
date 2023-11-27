683f13c7d6984761a37e19018a078847a943ec1f
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-27 10:52:55 +0200

    loginscreen init

---
+++ components/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './Login'
+
+export default function HomeScreen() {
+  return (
+    <Login />
+  );
+}
