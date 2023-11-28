9a3ca61d6680b56dc5ff1fba9f02ded14cf10f58
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-28 10:57:44 +0200

    calendar and add training -button added

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
 export default function HomeScreen() {
+  const [selectedDay, setSelectedDay] = useState('')
+
+  useEffect(() => {     // Set today to selected day
+    const today = new Date(); // Get this date
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // Months start from index 0
+    const day = today.getDate();
+
+    // Calendar library wants the date to be formatted like this (YYYY-MM-DD)
+    //const formattedDate = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
+    
+    const formattedDate = `${day}.${month}.${year}`
+
+    setSelectedDay(formattedDate);
+  }, []);
+
   return (
-    <Text>Tänään on pvm</Text>
+    <View>
+      <Calendar />
+      <Text>{selectedDay}</Text>
+      <Button title='Lisää treeni' onPress={() => console.log("tästä treenin lisäykseen")} />
+    </View>
   );
 }
