d104994f60715ef1fda9ca482de922bc6a706804
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-29 10:06:57 +0200

    saving and fetching data

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -2,9 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { storeData, getData } from './TrainingStorage'
+
+interface Workout {
+  liikunta: string;
+  kesto: string;
+}
 
 export default function HomeScreen() {
   const [selectedDay, setSelectedDay] = useState('')
+  const [trainings, setTrainings] = useState([])
 
   LocaleConfig.locales['fi'] = {
     monthNames: [
@@ -28,6 +35,11 @@ export default function HomeScreen() {
   };
   LocaleConfig.defaultLocale = 'fi';
 
+  const workouts = [
+    { liikunta: 'juoksu', kesto: '30 min' },
+    { liikunta: 'voimaharjoittelu', kesto: '45 min' },
+  ];
+
   useEffect(() => {     // Set today to selected day
     const today = new Date(); // Get this date
     const year = today.getFullYear();
@@ -38,18 +50,54 @@ export default function HomeScreen() {
     //const formattedDate = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
     
     const formattedDate = `${day}.${month}.${year}`
-
-    setSelectedDay(formattedDate);
+    console.log("täällä")
+    setSelectedDay(formattedDate);    
   }, []);
+
+  useEffect(() => {   //get trainings for the selected day
+    const getTrainings = (async () => {
+      console.log("täälläkin")
+      const response = await getData(selectedDay)
+      console.log("response:", response)
+      setTrainings(response)
+    })
+    getTrainings()
+  }, [selectedDay])
+
+  const handleStoreData = async () => {
+    try {
+      await storeData(selectedDay, workouts);
+      console.log('Treenit tallennettu onnistuneesti');
+    } catch (error) {
+      console.error('Virhe tallennuksessa:', error);
+    }
+  };
+
+  const showTrainings = () => {
+    if (trainings !== null) {
+        return trainings.map((t: Workout) => <Text>{t.liikunta}</Text>)
+     } else {
+        return <Text>Ei vielä treenejä tälle päivälle</Text>
+    }
+  }
+
+  const handleDayChange = (day: string) => {
+    console.log("day:", day)
+    const parsedDate = new Date(day); // Parsi päivämäärä merkkijonosta
+    const formattedDate = `${parsedDate.getDate()}.${parsedDate.getMonth() + 1}.${parsedDate.getFullYear()}`;
+
+    setSelectedDay(formattedDate)
+  }
 
   return (
     <View>
       <Calendar 
         firstDay={1}  // Week starts from Monday
-        onDayPress={day => setSelectedDay(day.dateString)}
+        onDayPress={(day) => handleDayChange(day.dateString)}
       />
       <Text>{selectedDay}</Text>
-      <Button title='Lisää treeni' onPress={() => console.log("tästä treenin lisäykseen")} />
+      {showTrainings()}
+      <Button title='Lisää treeni' onPress={handleStoreData} />
     </View>
   );
 }
---
+++ components/TrainingStorage.ts
@@ -0,0 +1,38 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+interface Workout {
+  liikunta: string;
+  kesto: string;
+}
+
+export const storeData = async (date: string, workouts: Workout[]): Promise<void> => {
+    try {
+      const jsonWorkouts = JSON.stringify(workouts);
+      await AsyncStorage.setItem(date, jsonWorkouts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+export const getData = async(dateToRetrieve: string) => {
+    try {
+      const res = await AsyncStorage.getItem(dateToRetrieve)
+
+      if(res){
+        try {
+          const json = JSON.parse(res);
+          console.log('json:', json);
+          return json;
+        } catch (error) {
+          console.error('Invalid JSON:', error);
+          return null;
+        }
+      } else {
+        console.log('Data is null or undefined.');
+        return null;
+      }
+    } catch (error) {
+      console.error('AsyncStorage getItem error:', error);
+      return null;
+      }
+  }
